2b8bea768f9572aa4f897df75f45701bfd74f000
Simion <simion.agv@gmail.com>
2014-03-14 23:28:41 +0200

    Added the initial plugin

---
+++ Slack.py
@@ -0,0 +1,126 @@
+import sublime
+import sublime_plugin
+import threading
+
+from .api import api_call
+
+
+API_CHANNELS = 'https://slack.com/api/channels.list'
+API_POST_MESSAGE = 'https://slack.com/api/chat.postMessage'
+
+
+class BaseSend(sublime_plugin.TextCommand):
+    """ Base class which shows a channels list and sends messages """
+
+    def __init__(self, view):
+        super(BaseSend, self).__init__(view)
+
+        # here we will store all messages
+        self.messages = []
+
+        # this channels list is only populated once, per sublime session
+        # you need to restart sublime to re-take channels from API
+        self.channels = []
+
+        # load plugin settings
+        self.settings = sublime.load_settings("Slack.sublime-settings")
+
+    def run(self, view):
+        # reset older messages stored in memory
+        self.messages = []
+        # check if token is set
+        if not self.settings.get('token'):
+            raise Exception('SLACK: Error! Please set your API "token" in Preferences -> Package Settings -> Slack -> Settings - User')
+
+    def on_select_channel(self, index):
+        if index is -1:
+            return sublime.status_message('SLACK: sending cancelled')
+
+        channel = self.channels[index]
+        sublime.status_message("Sending selection to: " + channel.get('name'))
+
+        username = self.settings.get('username')
+        info = sublime.platform()
+        try:
+            import getpass
+            info = "{0}, {1}".format(getpass.getuser(), info)
+        except:
+            # cannot get current OS user
+            pass
+
+        for message in self.messages:
+            args = {
+                'token': self.settings.get('token'),
+                'channel': channel.get('id'),
+                'text': message,
+                'username': "{0} ({1})".format(username, info)
+            }
+            response = api_call(API_POST_MESSAGE, args)
+            if response['ok']:
+                sublime.status_message('SLACK: Message sent successfully!')
+            else:
+                sublime.status_message('SLACK: Unexpected error!')
+
+    def init_message_send(self):
+        # check is channels are cached in memory
+        if not self.channels:
+            print('channels taken from API')
+            # get the channels
+            response = api_call(API_CHANNELS, {
+                'token': self.settings.get('token')
+            })
+            self.channels = response['channels']
+        else:
+            print('channels taken from MEMORY')
+
+        # build a list with channel names, to use for menus
+        channels = []
+        for channel in self.channels:
+            if not channel['is_archived']:
+                channels.append('#' + channel['name'])
+
+        # display a popup and let the user pick a channel
+        self.view.window().show_quick_panel(
+            channels,
+            self.on_select_channel)
+
+
+class SendSelectionCommand(BaseSend):
+    """ Send the selected text to slack. Multiple selections supported """
+    def run(self, view):
+        super(SendSelectionCommand, self).run(view)
+        # get all selected regions
+        for region in self.view.sel():
+            text = self.view.substr(region)
+
+            if not text:
+                sublime.status_message("SLACK: Error! No text selected")
+                return
+            self.messages.append(text)
+
+        threading.Thread(target=self.init_message_send).start()
+
+
+class SendMessageCommand(BaseSend):
+    """ Send a message from user input """
+
+    def run(self, view):
+        super(SendMessageCommand, self).run(view)
+        self.view.window().show_input_panel(
+            "Enter a message", '', self.on_done, None, None)
+
+    def on_done(self, message):
+        if not message:
+            return sublime.status_message('ERROR: Please enter a message')
+        self.messages = [message]
+        threading.Thread(target=self.init_message_send).start()
+
+
+class SendFileCommand(BaseSend):
+    """ Under development """
+    def on_select_channel(self, index):
+        if index is -1:
+            return sublime.status_message('SLACK: sending cancelled')
+
+    def run(self, view):
+        print(self.view.file_name())
---
+++ api.py
@@ -0,0 +1,33 @@
+import json
+import subprocess
+
+try:
+    from urllib.parse import urlencode
+    from urllib.request import urlopen
+except ImportError:
+    from urllib import urlencode, urlopen
+
+
+def api_call(url, args={}):
+    try:
+        response = urlopen(
+            url=url,
+            data=urlencode(args)
+        ).read().decode('utf8')
+    except:
+        URL = url + "?" + urlencode(args)
+        print('calling:', URL)
+        proc = subprocess.Popen(
+            ['curl', '-s', URL],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        out, err = proc.communicate()
+
+        response = out.decode('utf8')
+
+    response = json.loads(response)
+    if not response['ok']:
+        print(response['error'])
+        return False
+
+    return response
